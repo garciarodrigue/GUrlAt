d7881814d7a47894e3af7ca1e2ed8b4dc1d7957d
Tenshi Kira <garciarodrigueza587@gmail.com>
2024-01-07 12:57:29 -0600

    V1.1

--- GurlAt.py
+++ GurlAt.py
@@ -2,6 +2,7 @@ import os
 import requests
 import random
 import string
+import re
 from colorama import Fore, Back, Style, init
 from urllib.parse import urlparse
 
@@ -28,9 +29,33 @@ def verificar_estado_url(url, archivo):
                 azul = Fore.BLUE
                 verde = Fore.GREEN
                 print(f"{azul}GT {verde}Encontrado")
-                os.system(f"dirb {url}")
-            with open(archivo, "a") as file:
-                file.write(f"{url}\n")
+                os.system(f"dirb {url} -o {url}.txt")
+                
+                def filtrar_directorios_status_200(archivo):
+                    with open(archivo, 'r') as file:
+                        lineas = file.readlines()
+
+                    directorios_status_200 = []
+                    for linea in lineas:
+                        # Buscar líneas que contengan "Status: 200"
+                        if re.search(r'\[200\]', linea):
+                            # Extraer el directorio de la línea
+                            match = re.search(r'\[200\] (.+)', linea)
+                            if match:
+                                directorio = match.group(1)
+                                directorios_status_200.append(directorio.strip())
+
+                    return directorios_status_200
+
+                archivo_resultado = 'resultado.txt'
+                directorios_status_200 = filtrar_directorios_status_200(archivo_resultado)
+
+                # Mostrar los directorios con status 200
+                for directorio in directorios_status_200:
+                    print(directorio)
+                        
+                with open(archivo, "a") as file:
+                    file.write(f"{url}\n")
                 
     except requests.RequestException as e:
        pass
